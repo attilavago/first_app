bcc931f68e6e0fa3d94d2ad0bf75917fc0eb8d8a
Attila Vago <attila@attila-VirtualBox.(none)>
2013-12-26 22:14:47 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '223c91b0f27cb2f8c23873d3335cb11cfd8b1dba44e3feaaa26fa8640a54a7a5ba1994819d3ab624ddb3c01bb181d92af9ff5833ae7ffce833ffc9a12cc092b1'
